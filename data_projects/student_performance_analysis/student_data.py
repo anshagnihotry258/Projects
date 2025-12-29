d6613ca591e2eeb6d766ac3a09855fad5e6bfb05
anshagnihotry258 <anshagnihotry2@gmail.com>
2025-12-29 19:43:48 +0530

    Add student data for performance analysis

---
+++ data_projects/student_performance_analysis/student_data.py
@@ -0,0 +1,6 @@
+students = [
+    {"name": "Amit", "marks": [78, 85, 90]},
+    {"name": "Riya", "marks": [88, 92, 79]},
+    {"name": "Kunal", "marks": [60, 65, 70]},
+    {"name": "Sneha", "marks": [95, 91, 93]}
+]
